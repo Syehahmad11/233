099307699caf9a224f09b8780bc3a17e16b704c2
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-18 19:39:47 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -15,8 +15,8 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'zifabotz tidak aktif' : banned ? 'kamu dibanned' : '*APAAN NGETAG",ADA BANSOS KAH🗿?*',
-                '©zifabotz',
+                isBanned ? 'BotJr tidak aktif' : banned ? 'kamu dibanned' : '*APAAN NGETAG",ADA BANSOS KAH🗿?*',
+                '©JarotBotz',
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
                 m.isGroup ? 'Ban' : isBanned ? 'Unban' : 'Donasi',
@@ -31,14 +31,14 @@ handler.all = async function (m, { isBlocked }) {
         this.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `
 ╭═══ 〔 𝐋𝐈𝐒𝐓 𝐁𝐄𝐑𝐋𝐀𝐍𝐆𝐆𝐀𝐍𝐀𝐍 〕 ════
 ├⬡ 𝟏 ᗷᑌᒪᗩᑎ  *15.000*
-├⬡ 4 ᗷᑌᒪᗩᑎ  *30.000*
-├⬡ 8 BULAN   *40.000*
+├⬡ 4 ᗷᑌᒪᗩᑎ  *40.000*
+├⬡ 8 BULAN   *70.000*
 └═══════════════
 𝐌𝐚𝐮 𝐬𝐞𝐰𝐚? 𝐘𝐚 𝐥𝐚𝐧𝐠𝐬𝐮𝐧𝐠 𝐤𝐞 𝐨𝐰𝐧𝐞𝐫 𝐚𝐣𝐚 𝐧𝐠𝐚𝐩𝐢𝐧 𝐤𝐞 𝐛𝐨𝐭❗️
 ╭═══════════════════════
 ║╭──❉ 〔 𝐕𝐈𝐀 𝐌𝐎𝐁𝐈𝐋𝐄 〕 ❉──────
-║│➸ *DANA* : 085828764046
-║│➸ *GOPAY*: 085828764046
+║│➸ *DANA* : 085850539404
+║│➸ *PULSA*: 085850539404
 ║╰─────────
 ▌│█║▌║▌║║▌║▌║█│▌▌│█║▌║▌║║▌║▌║█│
 ╰═══════════════════════`.trim(), '©zifabotz', 'PAYMENT', '.payment', 'MENU', '.menu', m)
@@ -71,7 +71,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`time ${uptime} | 🖥️Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} |zifabotz by.rozi`).catch(_ => _)
+        await this.setStatus(`time ${uptime} | 🖥️Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} |BotJr by.Jarot`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
