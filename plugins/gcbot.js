f4622562426f0991128965b2c78cc43bf15dcd3e
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-18 20:16:02 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -7,10 +7,10 @@ let handler = async(m, { conn }) => {
         buttonText: 'pilih group',
         description: kontol,
         sections:  [{title: "groub pilihan", rows: [
-        {title: '⋮☰group zifa1', description: "📣join kuy", rowId:".zifa1"},
-        {title: '⋮☰group zifa2', description: "📣join kuy", rowId:".zifa2"},
-        {title: '⋮☰group haori', description: "📣join kuy", rowId:".haori1"},
-        {title: '⋮☰group nekel', description: "📣join kuy", rowId:".nekel1"},
+        {title: '⋮☰group Jarot1', description: "📣join kuy", rowId:".Jarot1"},
+        {title: '⋮☰group Jarot2', description: "📣join kuy", rowId:".Jarot2"},
+        {title: '⋮☰group KOSONG', description: "📣join kuy", rowId:".gada"},
+        {title: '⋮☰group KOSONG', description: "📣join kuy", rowId:".gada"},
        ] }],
         listType: 1
        }
