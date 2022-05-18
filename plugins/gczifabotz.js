35737f095bad2c49ceb1748194ce1f13e3072e1a
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-18 21:49:32 +0700

    Update gczifabotz.js

--- plugins/gczifabotz.js
+++ plugins/gczifabotz.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.botwm
 let str = `
-*Group Jarotbotz1 📮:https://chat.whatsapp.com/E5l0ofPuMxG3H8ZhMRyI7e*
+*Group Jarotbotz1 📮:https://chat.whatsapp.com/E5l0ofPuMxG3H8ZhMRyI7e *
 `.trim()
 conn.sendButton(m.chat, str, `${botol}`, `⋮☰ Menu`, `.menu`, m)
 }
