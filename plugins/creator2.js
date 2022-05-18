623ae68ba184febb9c9a5c7cf3caac3106cd1a9a
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-18 20:04:23 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -4,16 +4,16 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 
 let str = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : ROZI
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
+*💌 Nama* : JAROT
+*🎨 Umur* : 18
+*🧮 Kelas* : lulus
 *📈 Status* : Busy
 *───────[ SOSIAL MEDIA ]───────*
 *📷 instagran* : zalfapontianak
 *🇫  Facebook* : -
 *🏮 Chanel Youtube* : -
-  ⬣━〔Powered By Rozi〕━⬣
-©️ zifabotz`
+  ⬣━〔Powered By JAROT〕━⬣
+©️ JAROTbotz`
 let name = await conn.getName(m.sender)
 
 let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
@@ -25,14 +25,14 @@ let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ?
       "product": {
         "productImage": image,
         "productId": "4938174216214248",
-        "title": "OWNER ZIFABOTZ",
+        "title": "OWNER BOTJR",
         "description": '\n' + wm + '\n' + str,
-        "retailerId": "Thanks For zifabotz !",
+        "retailerId": "Thanks For BotJr !",
         "url": "▌│█║▌║▌║║▌║▌║█│▌",
         "descriptionCount": "999999999",
         "productImageCount": "1",
       },
-      "businessOwnerJid": "6285828764046@s.whatsapp.net",
+      "businessOwnerJid": "6285850539404@s.whatsapp.net",
       "contextInfo": {
         "forwardingScore": 9999,
         "isForwarded": true
